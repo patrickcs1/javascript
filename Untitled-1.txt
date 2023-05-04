4a0f6ab48f5d99ba57fd1a8a821a4ec209234404
patrickcs1 <patrickam_marques@outlook.com>
2023-05-04 17:19:13 -0300

    primeiro commit calculadora

---
+++ Untitled-1.txt
@@ -0,0 +1,6 @@
+let varteste: number | string[];
+
+varteste = 10;
+varteste = ["teste1","teste2"]
+
+console.log(varteste);
---
+++ index.js
@@ -0,0 +1,25 @@
+//let tabuada = 5;*/
+
+/*for(let i = 0; i <=10 ;i++){
+    console.log(`${i} X ${tabuada} = ${i*tabuada}`)
+}*/
+
+
+/*let soma = (x1,x2) => {
+  return x1 * x2;
+}
+
+console.log(soma(2,10))*/
+
+/*window.addEventListener('focus',event =>{
+  console.log("focus")
+})
+
+
+
+window.addEventListener('click', event => {
+  console.log("click")
+})*/
+
+let agora = new Date();
+console.log(agora.toLocaleDateString('pt-br'));
